fd91a0292e06659670fdf29be4633873d45bc736
swtiit <doanhnoi161.dev@gmail.com>
2017-07-11 18:36:00 +0700

    Fix php.ini

--- sayhi.php
+++ sayhi.php
@@ -1,6 +1,12 @@
-<?php 
+<?php
 	if (isset($_POST['login'])) {
 		$email = $_POST['email'];
-		echo "Wellcome to: ".$email;
+		$password = $_POST['password'];
+
+		if ($email == 'admin@gmail.com' && $password == 'admin') {
+			echo "Welcome to Summoner's Rift";
+		} else {
+			echo "Login fail!!!";
+		}
 	}
 ?>
